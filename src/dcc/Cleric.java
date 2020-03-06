546fc1e78a4de3da3e96987fe8b7bfdf21383a9e
Erik <Erik@158.37.235.170>
2020-03-06 11:03:16 +0100

    06.03

--- src/dcc/Cleric.java
+++ src/dcc/Cleric.java
@@ -1,5 +1,9 @@
 package dcc;
 
+import javax.swing.JOptionPane;
+
+import tools.Diceroller;
+
 public class Cleric extends CharacterDCC{
 	public String god;
 	public String[] spellbook;
@@ -8,6 +12,10 @@ public class Cleric extends CharacterDCC{
 	public Cleric(int str, int agi,int sta, int per,int itl, int lck, int hp, int maxHP, Armor armor, int ac, boolean shield, int randomizer, String name, int money,Weapon weapon) {
 		super(str, agi,sta, per,itl, lck, hp, maxHP, armor, ac, shield, randomizer, name, money,weapon); 
 		occupation = "Cleric";
+		gainHp(8);
+		lvl = 1;
+		god = JOptionPane.showInputDialog("Choose your god");
+		spellbook = divineSpells(god);
  
 }
 	public Cleric(int str, int agi,int sta, int per,int itl, int lck, int hp, int maxHP, Armor armor, int ac, boolean shield, int randomizer, String name, int money,Weapon weapon,int xp,int atkMod,int lvl) {
@@ -16,5 +24,21 @@ public class Cleric extends CharacterDCC{
 }
 	public String toString() {
 		return name + " the Cleric , lvl " + lvl + " wielding a " + weapon.getName() + " wearing " + armor.getName() + " with " +maxHp + " hp ";
+	}
+	
+	public String[] divineSpells(String god) {
+		return null;
+	}
+	
+	public void levelUp() {
+		gainHp(8);
+	}
+	public void heal(CharacterDCC friend) {
+		int roll = Diceroller.dx(1,8);
+		friend.setHp(friend.getHp() + roll);
+		if (friend.getHp() > friend.getMaxHp()) {
+			friend.setHp(friend.getMaxHp());
+		}
+		
 	}
 }
