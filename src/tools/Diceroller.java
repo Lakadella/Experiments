a40b71f7e3590f4792275bb60d0f6e109deb512f
Erik <h586622@stud.hvl.no>
2021-03-19 15:40:19 +0100

    fikset terninger

--- src/tools/Diceroller.java
+++ src/tools/Diceroller.java
@@ -12,7 +12,7 @@ public class Diceroller {
 	public static int d4(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d4 = (int) Math.round((Math.random() * 4) + 0.5);
+			int d4 = (int) Math.round((Math.random() * 4) + 1);
 			sum = sum + d4;
 		}
 
@@ -23,7 +23,7 @@ public class Diceroller {
 	public static int d6(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d6 = (int) Math.round((Math.random() * 6) + 0.5);
+			int d6 = (int) Math.round((Math.random() * 6) + 1);
 			sum = sum + d6;
 		}
 
@@ -34,7 +34,7 @@ public class Diceroller {
 	public static int d8(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d8 = (int) Math.round((Math.random() * 8) + 0.5);
+			int d8 = (int) Math.round((Math.random() * 8) + 1);
 			sum = sum + d8;
 		}
 
@@ -45,7 +45,7 @@ public class Diceroller {
 	public static int d10(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d10 = (int) Math.round((Math.random() * 10) + 0.5);
+			int d10 = (int) Math.round((Math.random() * 10) + 1);
 			sum = sum + d10;
 		}
 
@@ -56,7 +56,7 @@ public class Diceroller {
 	public static int d12(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d12 = (int) Math.round((Math.random() * 12) + 0.5);
+			int d12 = (int) Math.round((Math.random() * 12) + 1);
 			sum = sum + d12;
 		}
 
@@ -67,7 +67,7 @@ public class Diceroller {
 	public static int d20(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d20 = (int) Math.round((Math.random() * 20) + 0.5);
+			int d20 = (int) Math.round((Math.random() * 20) + 1);
 			sum = sum + d20;
 		}
 
@@ -77,7 +77,7 @@ public class Diceroller {
 	public static int d100(int antall) {
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int d100 = (int) Math.round((Math.random() * 100) + 0.5);
+			int d100 = (int) Math.round((Math.random() * 100) + 1);
 			sum = sum + d100;
 		}
 
@@ -88,7 +88,7 @@ public class Diceroller {
 		int x = sider;
 		int sum = 0;
 		for (int i = 0; i < antall; i++) {
-			int dx = (int) Math.round((Math.random() * x) + 0.5);
+			int dx = (int) Math.round((Math.random() * x) + 1);
 			sum = sum + dx;
 		}
 
